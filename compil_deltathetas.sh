70f040c2780204ae4c78072ae4bcca1bf4b4af79
Léo Girardin <elvith@niil.fr>
2012-05-31 14:55:56 +0200

    Nettoyage

--- compil_deltathetas.sh
+++
@@ -1,2 +0,0 @@
-
-gcc main_deltathetas.c matrices.c params_mvt.c calc_deltatheta.c charger_image.c aux.c -D ETUDE_STAT -o deltathetas -lm -lSDL -lSDL_image -std=c99 -Wall
--- compil_u2.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-gcc main_u2.c charger_image.c -o u2 -lm -lSDL -lSDL_image -std=c99 -Wall
-
-
