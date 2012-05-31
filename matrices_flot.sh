d5cbccd0e766a05975b7f0c0abee36422a02650b
David Lafontaine <lafontaine@crans.org>
2012-05-31 14:12:52 +0200

    menage

--- matrices_flot.sh
+++ matrices_flot.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-./parametres $1 $2 $3 > params_mvt_cam
+parametres $1 $2 $3 > params_mvt_cam
 P=$(/bin/cat params_mvt_cam)
 
-./u1 $P $3 $1 > flot_vertical
-./u2 $P $3 $1 > flot_horizontal
+u1 $P $3 $1 > flot_vertical
+u2 $P $3 $1 > flot_horizontal
+plambda flot_horizontal flot_vertical "x y join" > $4
 
