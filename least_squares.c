4ecd12312536f3765c9eaf1c7105cdf79f07865e
Léo Girardin <elvith@niil.fr>
2012-06-05 15:09:23 +0200

    maj

--- least_squares.c
+++ least_squares.c
@@ -19,46 +19,40 @@ mat least_squares_quad_form(int size_x, int size_y) {
 			Q.m[i][j] = 0.0;
 	}	
 
-	for(x=0;x<size_x;x++) {
-		for(y=0;y<size_y;y++) {
+    vect e, f;
+    e.size = 6;
+    f.size = 6;
 
-			Q.m[0][0] += (x*x + y*y);
-			Q.m[0][2] += (x);
-			Q.m[0][3] += (y);
-			Q.m[0][4] += (x*x*x + x*y*y);
-			Q.m[0][5] += (y*y*y + y*x*x);				
-			
-			Q.m[1][1] += (x*x + y*y);
-			Q.m[1][2] += (y);
-			Q.m[1][3] += (-x);
 
-			Q.m[2][0] += (x);
-			Q.m[2][1] += (y);
-			Q.m[2][2] += (1.0);
-			Q.m[2][4] += (x*x);
-			Q.m[2][5] += (x*y);
+	for(x = 0; x<size_x; x++){
+        for(y = 0; y<size_y; y++) {
 
-			Q.m[3][0] += (y);
-			Q.m[3][1] += (-x);
-			Q.m[3][3] += (1.0);
-			Q.m[3][4] += (x*y);
-			Q.m[3][5] += (y*y);
+            int x_ = x - size_x/2;
+            int y_ = y - size_y/2;
 
-			Q.m[4][0] += (x*x*x + x*y*y);
-			Q.m[4][2] += (x*x);
-			Q.m[4][3] += (x*y);
-			Q.m[4][4] += (x*x*x*x + x*x * y*y);
-			Q.m[4][5] += (x*x*x*y + y*y*y*x);
-			 
-			Q.m[5][0] += (x*x*y + y*y*y);
-			Q.m[5][2] += (x*y);
-			Q.m[5][3] += (y*y);
-			Q.m[5][4] += (x*x*x*y+y*y*y*x);
-			Q.m[5][5] += (y*y*y*y + x*x*y*y);
-		}
-	}
-	
-	return Q;
+            e.v[0] = x_;
+            e.v[1] = y_;
+            e.v[2] = 1;
+            e.v[3] = 0;
+            e.v[4] = x_*x_;
+            e.v[5] = x_*y_;
+
+            f.v[0] = y_;
+            f.v[1] = -x_;
+            f.v[2] = 0;
+            f.v[3] = 1;
+            f.v[4] = x_*y_;
+            f.v[5] = y_*y_;
+            
+            for(i=0;i<6;i++){
+                for(j=0;j<6;j++)
+                    Q.m[i][j] += (e.v[i]*e.v[j] + f.v[i]*f.v[j]);
+            }
+
+        }
+    }
+   
+    return Q;
 }
 
 /* Compute the vector of the linear form associated to the least squares problem that we have to solve */
@@ -78,12 +72,15 @@ vect least_squares_lin(int size_x, int size_y, double** flow_x, double** flow_y)
 			double a = flow_x[x][y];
 			double b = flow_y[x][y];
 
-		  	l.v[0] +=  (a*x + b*y);
-			l.v[1] +=  (a*y - b*x);
+            int x_ = x - size_x/2;
+            int y_ = y - size_y/2;
+
+		  	l.v[0] +=  (a*x_ + b*y_);
+			l.v[1] +=  (a*y_ - b*x_);
 			l.v[2] +=  (a);
 			l.v[3] +=  (b);
-			l.v[4] +=  (a*x*x + b*x*y);
-			l.v[5] +=  (b*y*y + a*x*y); 
+			l.v[4] +=  (a*x_*x_ + b*x_*y_);
+			l.v[5] +=  (b*y_*y_ + a*x_*y_); 
 		}
 	}
 	
@@ -94,20 +91,18 @@ vect least_squares_lin(int size_x, int size_y, double** flow_x, double** flow_y)
 }
 
 // Compute an approximation of the motion parameters using the optical flow
-params least_squares_params(int size_x, int size_y, double** flow_x, double** flow_y){
+vect least_squares_params(int size_x, int size_y, double** flow_x, double** flow_y){
 
-	mat U = least_squares_quad(size_x, size_y);
-	vect B = least_squares_lin(size_xsize_y, flow_x, flow_y);
+	mat U = least_squares_quad_form(size_x, size_y);
+	vect B = least_squares_lin(size_x, size_y, flow_x, flow_y);
         
 	mat L = cholesky(U);
- 	
+
 	int i;
 	for(i=0;i<6;i++)
 		B.v[i] *= (-0.5);
 
-	vect C = res_upper_tri(L, res_lower_tri(transpose(L), B));
-	params p = {C.v[0], C.v[1], C.v[2], C.v[3], C.v[4], C.v[5]};     	
+	return res_upper_tri(L, res_lower_tri(transpose(L), B));
 	
-	return p;
 }
 
--- least_squares.h
+++ least_squares.h
@@ -1,7 +1,7 @@
 
 /* least_squares.h */
 // Compute an appoximation of (a1,a2,c1,c2,q1,q2) as they are described in the Jonchery thesis with a least squares method
-
+// and an input flow computed with a classical method
 
 #ifndef LEAST_SQUARES
 #define LEAST_SQUARES
@@ -9,21 +9,14 @@
 
 #include "matrix.h"
 
-// Camera motion parameters
-typedef struct {
-
-	double a1,a2,c1,c2,q1,q2;
-
-} params;
-
 // Quadratic form of the least squares method
-mat least_squares_quad(int size_x, int size_y); 
+mat least_squares_quad_form(int size_x, int size_y); 
 
 // Linear form of the least squares method
 vect least_squares_lin(int size_x, int size_y, double** flow_x, double** flow_y); 
 
 // Final computation
-params least_squares_params(int size_x, int size_y, double** flow_x, double** flow_y);
+vect least_squares_params(int size_x, int size_y, double** flow_x, double** flow_y);
 
 
 #endif
