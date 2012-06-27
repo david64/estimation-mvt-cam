e1b9e64b4d9333c38fb900dc775fee172c7bca91
Léo Girardin <elvith@niil.fr>
2012-06-27 15:29:47 +0200

    GPL

--- flow_input.c
+++ flow_input.c
@@ -1,3 +1,19 @@
+/* Camera motion estimation, by Léo Girardin and David Lafontaine
+    Copyright (C) 2012 Léo Girardin and David Lafontaine
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #include "motion_params.h"
 #include <stdlib.h>
